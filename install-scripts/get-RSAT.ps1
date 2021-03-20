9293bae641dd33887374fb7dd32c4bbaad3e90ec
eamon <emulholland@fortifiedhealthsecurity.com>
2021-03-20 13:35:27 -0400

    auto commit by gitter function

--- install-scripts/get-RSAT.ps1
+++ install-scripts/get-RSAT.ps1
@@ -1,3 +1,6 @@
+# Author: Gregory for Microsoft
+# Source: https://community.spiceworks.com/how_to/164370-install-rsat-tools-on-windows-10-1903-via-powershell
+
 $Install = Get-WindowsCapability -Online | Where-Object {$_.Name -like "Rsat*" -AND $_.State -eq "NotPresent"}
 
 $Install | ForEach-Object {
