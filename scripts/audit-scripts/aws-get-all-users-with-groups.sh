59ce0c760c5de62f90ae1560aafab607c9a18acd
eamon <eamon.mulholland@timelycare.com>
2023-09-18 13:15:37 -0500

    auto commit by gitter function

---
+++ scripts/audit-scripts/aws-get-all-users-with-groups.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This script is for auditing AWS users and their group memberships. It requires the AWS CLI to already be set up and authenticated, as well as for you to have the relevant AWS permissions to view groups for other users. It also requires jq for parsing JSON.
+
+for u_name in $(aws iam list-users --output json | jq -r '.Users[].UserName') ; do
+  groups="$(aws iam list-groups-for-user \
+    --user-name ${u_name} \
+    | cut -w -f 5 \
+    | tr '\n' ' ')"
+  echo "${u_name}, ${groups}" >> aws_users_with_groups.csv
+done
