8f80001f39fde2a206618c70e27b10ca307afcf4
eamon <eamon.mulholland@timelycare.com>
2024-01-25 15:35:03 -0600

    auto commit by gitter function

--- scripts/audit-scripts/gitleaks_wrapper.sh
+++ scripts/audit-scripts/gitleaks_wrapper.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-# this script is a wrapper around https://github.com/gitleaks/gitleaks which enables results to be suppressed via a whitelist of false positives
-# it's intended for situations where the normal suppression by Fingerprint doesn't work because of dynamic filenames or something, such as when scanning a web bundle in a CI/CD pipeline
 
 # accept gitleaks report as an argument
 if [ -z "$1" ]
 then
-	echo "[*] Usage    : $0 <gitleaks json report>"
+	echo "[*] Usage    : $0 <gitleaks json report> <output format (table or list)>"
 exit 0
 fi
 
@@ -14,6 +12,7 @@ WHITELIST_FILE="whitelist.txt"
 suppressed_issues=()
 ISSUE_NUM=0
 gitleaks_report_json_path="$1"
+output_format="$2"
 FILTERED_ISSUES_FILE="gitleaks_filtered_issues.json"
 
 # loop through every issue in the report
@@ -37,9 +36,21 @@ while read -r; do
         # if it is we just skip it and do nothing
         true
     else
-        # if it's not then that means this finding isn't in the whitelist and we should print all the details for it
-        jq ".[$ISSUE_NUM]" "$gitleaks_report_json_path" >> $FILTERED_ISSUES_FILE
-        echo ',' >> $FILTERED_ISSUES_FILE
+        if [ "$output_format" = "table" ]; then
+            # if it's not then that means this finding isn't in the whitelist and we should print all the details for it
+            jq ".[$ISSUE_NUM]" "$gitleaks_report_json_path" >> $FILTERED_ISSUES_FILE
+            echo ',' >> $FILTERED_ISSUES_FILE
+        else
+            DESCRIPTION=$(jq -r ".[$ISSUE_NUM] | .Description" "$gitleaks_report_json_path")
+            LINE=$(jq -r ".[$ISSUE_NUM] | .StartLine" "$gitleaks_report_json_path")
+            FILE=$(jq -r ".[$ISSUE_NUM] | .File" "$gitleaks_report_json_path")
+
+            echo "Description: $DESCRIPTION"
+            echo "Line: $LINE"
+            echo "File: $FILE"
+            echo "Secret: $current_finding"
+            echo ""
+        fi
     fi
 
     # Increment the issue number so we can keep track of where we are in the
@@ -49,16 +60,21 @@ while read -r; do
 
 done < <(jq -c '.[]' "$gitleaks_report_json_path")
 
-# fix some formatting on the filtered issues file so it's a proper json array
-sed -i '' '$ s/.$//' $FILTERED_ISSUES_FILE
-echo '[' | cat - $FILTERED_ISSUES_FILE > temp && mv temp $FILTERED_ISSUES_FILE
-echo ']' >> $FILTERED_ISSUES_FILE
+if [ "$output_format" = "table" ]; then
+    # fix some formatting on the filtered issues file so it's a proper json array
+    sed -i '' '$ s/.$//' $FILTERED_ISSUES_FILE
+    echo '[' | cat - $FILTERED_ISSUES_FILE > temp && mv temp $FILTERED_ISSUES_FILE
+    echo ']' >> $FILTERED_ISSUES_FILE
 
-# output the filteres issues file as a nicely formatted table
-cat $FILTERED_ISSUES_FILE | jq -r '(["RULE","SECRET","FILE","LINE"] | (.,map(length*"-"))), (.[] | [.RuleID, .Secret, .File, .StartLine]) | @tsv' | column -t
+    # output the filteres issues file as a nicely formatted table
+    cat $FILTERED_ISSUES_FILE | jq -r '(["RULE","SECRET","FILE","LINE"] | (.,map(length*"-"))), (.[] | [.RuleID, .Secret, .File, .StartLine]) | @tsv' | column -t
+fi
+
 
 # output a report on how many issues were suppressed by the whitelist
 printf "\n+++++++++\n"
 echo "${#suppressed_issues[*]} issues were suppressed by the whitelist: $WHITELIST_FILE"
 
-rm $FILTERED_ISSUES_FILE
+if [ "$output_format" = "table" ]; then
+    rm $FILTERED_ISSUES_FILE
+fi
