b5ae3e403704a5c7f9c54c22beafeb6f198e873f
Eamon <mulhollandeamon@gmail.com>
2024-10-21 16:37:01 -0500

    Update email_extractor.py

--- email_extractor/email_extractor.py
+++ email_extractor/email_extractor.py
@@ -11,10 +11,12 @@ def parse_arguments():
     parser.add_argument('-o', '--output-folder', type=str, default=".\output", help="Folder to save the Excel files (default: .\output)")
     parser.add_argument('-c', '--columns', type=str, default="2, 4, 5, 6, 7", help="Table columns to extract (remember the first column is 0)")
     parser.add_argument('-p', '--print', type=str, default="5", help="Table column(s) to print")
-    parser.add_argument('-e', '--email', type=str, default="user@outlook.com", help="Email address to use for the Outlook connection")
+    parser.add_argument('-e', '--email', type=str, default="YOUR_USER@outlook.com", help="Email address to use for the Outlook connection")
     parser.add_argument('-f', '--folder', type=str, default="Inbox", help="Email folder to scan")
     parser.add_argument('-l', '--limit', type=int, default=3, help="Number of emails to extract from (useful to shorten the output for readability or sanity testing)")
-    parser.add_argument('-q', '--quiet', action=argparse.BooleanOptionalAction, help="Quiet mode: suppresses banner and printing extracted data to terminal")
+    parser.add_argument('-q', '--quiet', action=argparse.BooleanOptionalAction, help="Quiet mode: suppresses status messages")
+    parser.add_argument('-a', '--additional-commands', action=argparse.BooleanOptionalAction, help="Print additional commands for easy copy/paste")
+    parser.add_argument('-b', '--banner', action=argparse.BooleanOptionalAction, help="Print the title banner")
 
     return parser.parse_args()
 
@@ -24,7 +26,6 @@ def print_banner(text, symbol):
     print(symbol * banner_length)
     print(symbol, text, symbol)
     print(symbol * banner_length)
-
 
 # Access a specified mailbox
 def connect_to_outlook(address, folder):
@@ -79,13 +80,13 @@ def save_to_excel(headers, data, column_indices, excel_file):
     filtered_df.to_excel(excel_file, header=False, index=False)
 
 # Print selected data to the terminal
-def print_columns_to_terminal(headers, data, columns_to_print):    
-    print(", ".join([row[i] for row in data for i in columns_to_print]))
+def format_data_for_terminal(headers, data, columns_to_print, separator):    
+    return separator.join([row[i] for row in data for i in columns_to_print])
 
 if __name__== "__main__":
     args = parse_arguments()
 
-    if not args.quiet:
+    if args.banner:
         print_banner("Email Extractor", "#")
         print("script by v01dlight")
         print()
@@ -97,7 +98,7 @@ if __name__== "__main__":
     columns_to_print = [int(item) for item in args.print.split(',')]
 
     # Filter messages by subject line matching a regex
-    target_subject = re.compile(r".*REGEX PATTERN TO MATCH.*", re.IGNORECASE)
+    target_subject = re.compile(r".*YOUR_PATTERN.*", re.IGNORECASE)
     #messages = messages.Restrict(f"[Subject] = '{target_subject}'")
 
     # Regex to match the date format in the subject (MM/DD/YYYY)
@@ -126,7 +127,7 @@ if __name__== "__main__":
 
                 # Check if the email has a subject line matching our regex, and doesn't have a flag indicating it should be skipped
                 if message.FlagStatus != 3 and message.FlagStatus != 1 and message.FlagStatus != 2 and re.search(target_subject, subject):
-                    print(f"[+] Processing email: {subject}")
+                    if not args.quiet: print(f"[+] Processing email: {subject}")
 
                     # debugging
                     #print(f"[!] Flag Status: {message.FlagStatus}")
@@ -142,20 +143,34 @@ if __name__== "__main__":
                         # Save the filtered data to Excel
                         excel_file = generate_excel_filename(subject, output_folder)
                         save_to_excel(headers, data, column_indices, excel_file)
-                        print(f"[>] Data saved to {excel_file}")
+                        if not args.quiet: print(f"[>] Data saved to {excel_file}")
 
                         # Mark the email as "Processed"
                         message.FlagStatus = 3
                         message.Save()
-                        print(f"[+] Email marked as processed")
-                        print()
 
-                        # print data to the terminal
-                        if not args.quiet:
-                            print_columns_to_terminal(headers, data, columns_to_print)
+                        if not args.quiet: print(f"[+] Email marked as processed\n")
+
+                        # print the addtional commands, prepopulated with extracted data
+                        if args.additional_commands:
+
+                            comma_separated_data = format_data_for_terminal(headers, data, columns_to_print, ", ")
+                            print(comma_separated_data)
                             print()
+
+                            if not args.quiet: print("[!] Run these additional commands:\n")
+
+                            commands = """
+egrep -i "{pipe_separated_data}" /YOUR_PATH/YOUR_FILE.txt
+
+do
+YOUR_COMMANDS
+done\n
+                            """.format(pipe_separated_data = format_data_for_terminal(headers, data, columns_to_print, "|"))
+                            print(commands)
 
                         # Increment the extraction counter
                         num_extracted += 1
                     else:
-                        print(f"[!] No table found in email")
+                        if not args.quiet: print(f"[!] No table found in email\n")
+
